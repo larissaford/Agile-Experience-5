85d657f34d0afa3d3aa46dfac5b98b74f9476e42
Larissa <larissa@coreyford.com>
2020-04-24 17:32:23 -0500

    updated student.php

--- Student.php
+++ Student.php
@@ -13,12 +13,61 @@
 		$LastName  = $_GET["LastName"];
 		$isActive= $_GET["isActive"];
 		
-		if(IsTrue($isActive) && IsSet($FirstName) && IsSet($LastName) && strlen($ID)==5){
-		
-			print "$LastName, $FirstName<br  />";
-			print "$ID";
-		}else
-			//TO-DO, link back to html: echo '<a href="http://mscs-php.uwstout.edu.html">Error: Return to your form to complete the data.</a>)';
+		//display labs of student
+		//display grades
+		//display classes
+
+		echo "<table style='border: solid 1px black;'>";
+		echo "<tr><th>FullName</th></tr>";
+
+		class TableRows extends RecursiveIteratorIterator {
+			function __construct($it) {
+				parent::__construct($it, self::LEAVES_ONLY);
+			}
+
+			function current() {
+				return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+			}
+
+			function beginChildren() {
+				echo "<tr>";
+			}
+
+			function endChildren() {
+				echo "</tr>" . "\n";
+			}
+		}
+
+		$servername = "https://144.13.22.59:3306";
+		$username = "g5AppUser";
+		$password = "aug5";
+		$dbname = "G5AgileExperience";
+
+		try {
+			$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$stmt = $conn->prepare("
+			select FirstName,LastName, StudentID, Class.Name className, Lab.Name labName, BeginDate, DueDate 
+			from Student
+			inner join Grade on Student.ID=Grade.StudentID
+			inner join Lab on Grade.LabID=Lab.ID
+			inner join SectionLab on Lab.ID=SectionLab.SectionID
+			inner join Section on SectionLab.SectionID=Section.ID
+			inner join Class on Section.ClassID=Class.ID
+			where Student.isActive=1 and Lab.IsActive=1");
+			$stmt->execute();
+
+			// set the resulting array to associative
+			$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+			foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+				echo $v;
+			}
+		}
+		catch(PDOException $e) {
+			echo "Error: " . $e->getMessage();
+		}
+		$conn = null;
+		echo "</table>";
 	?>
 	</p>
 </body>
