11415840710ba3b51fe3097163c4b08fb229f783
James Cerasani <fullmetalnerd97@gmail.com>
2020-04-24 15:29:21 -0500

    Initial table idea for E1.1.2.T1

---
+++ Database Schema and Scripts/SearchByLab.php
@@ -0,0 +1,17 @@
+<table style="width:100%">
+  <tr>
+    <th>Lab</th>
+    <th>Class</th>
+    <th>Section</th>
+  </tr>
+  <tr>
+    <td>Lab01</td>
+    <td>CS-144</td>
+    <td>002</td>
+  </tr>
+  <tr>
+    <td>Lab02</td>
+    <td>CS-248</td>
+    <td>003</td>
+  </tr>
+</table>
