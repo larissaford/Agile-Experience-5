ae502a61fc7c2665281f4e8fcc8513f93c4612fd
Larissa <larissa@coreyford.com>
2020-04-27 19:30:42 -0500

    nearly finished results.php

--- results.php
+++ results.php
@@ -19,25 +19,24 @@
 
 		//TEST
 		$filter = "StudentID";
-		$searchBy = 3;
+		$searchBy = "";
 
 		try {
-
-
 
 			//search by Lab.Name, SectionNum, Class.Name, FirstName, LastName, etc. 
 			$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 			$arr = [];
-			
+
 			$where="";
 			if(!empty($searchBy)){
 				$where .= "and ".$filter."= ".$searchBy;
 			}
-				
+			
+			/*
 			$stmt = $conn->prepare("
-				select distinct Lab.Name labName, BeginDate, DueDate, FirstName,LastName, StudentID, Class.Name className, Section.SectionNum section
+				select distinct Lab.Name labName, BeginDate, DueDate, FirstName,LastName, StudentID, Class.Name className, Section.SectionNum section, Grade
 				from Student
 				inner join Grade on Student.ID=Grade.StudentID
 				inner join Lab on Grade.LabID=Lab.ID
@@ -45,26 +44,115 @@
 				inner join Section on SectionLab.SectionID=Section.ID
 				inner join Class on Section.ClassID=Class.ID
 				where Student.isActive and Lab.IsActive and Class.IsActive $where");
+			*/
 			
-			$stmt->execute();
+			switch ($filter) {
+				case 'Lab.Name':
+					$stmt = $conn->prepare("
+						select distinct Lab.Name labName, BeginDate, DueDate, Class.Name className, Section.SectionNum section
+						from Lab on Grade.LabID=Lab.ID
+						inner join SectionLab on Lab.ID=SectionLab.SectionID
+						inner join Section on SectionLab.SectionID=Section.ID
+						inner join Class on Section.ClassID=Class.ID
+						where Lab.IsActive and Class.IsActive $where");
+						$stmt->execute();
+
+					echo "<table><tr><th>Lab Name</th><th>Lab begin date</th><th>Lab due date</th><th>Class Name</th><th>Class Section</th></tr>";
+					while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
+						$arr[] = $row;
+						list($labName, $BeginDate, $DueDate, $FirstName,$LastName, $StudentID, $className, $section, $grade) = $row;
+						echo "
+							<td><a href='lab.php?labName=".$labName."'>".$labName."</a></td>
+							<td>".$BeginDate."</td>
+							<td>".$DueDate."</td>
+							<td><a href='Class.php?className=".$className."'>".$className."</a></td>
+							<td><a href='Class.php?section=".$section."'>".$section."</a></td>
+							</tr>";
+							
+						/* outline for inserting links:
+						<td><a href='Student.php?StudentID=".$StudentID."'>".$StudentID."</a></td>
+						*/
+		
+					}
+					break;
+
+				case "Class.Name":
+					$stmt = $conn->prepare("
+						select distinct Class.Name className, Section.SectionNum section
+						from Section
+						inner join SectionLab SectionLab.SectionID=Section.ID
+						inner join Class on Section.ClassID=Class.ID
+						where Class.IsActive $where");
+						$stmt->execute();
+
+					echo "<table><tr><th>Class Name</th><th>Class Section</th></tr>";
+					while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
+						$arr[] = $row;
+						list($className, $section) = $row;
+						echo "
+							<td><a href='Class.php?className=".$className."'>".$className."</a></td>
+							<td><a href='Class.php?section=".$section."'>".$section."</a></td>
+							</tr>";
+						
+						/* outline for inserting links:
+						<td><a href='Student.php?StudentID=".$StudentID."'>".$StudentID."</a></td>
+						*/
+					}
+					break;
+
+				case "StudentID":
+				case "FirstName":
+				case "LastName":
+					$stmt = $conn->prepare("
+						select distinct FirstName,LastName, StudentID, Class.Name className, Section.SectionNum section
+						from Student
+						inner join Grade on Student.ID=Grade.StudentID
+						inner join Lab on Grade.LabID=Lab.ID
+						inner join SectionLab on Lab.ID=SectionLab.SectionID
+						inner join Section on SectionLab.SectionID=Section.ID
+						inner join Class on Section.ClassID=Class.ID
+						where Student.isActive and Class.IsActive $where");
+					$stmt->execute();
+					
+					echo "<table><tr><th>Student Name</th><th>Student ID</th><th>Class Name</th><th>Class Section</th></tr>";
+					while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
+						$arr[] = $row;
+						list($FirstName,$LastName, $StudentID, $className, $section) = $row;
+						echo "
+							<td><a href='Student.php?StudentID=".$StudentID."'>".$FirstName." ".$LastName."</a></td>
+							<td><a href='Student.php?StudentID=".$StudentID."'>".$StudentID."</a></td>
+							<td><a href='Class.php?className=".$className."'>".$className."</a></td>
+							<td><a href='Class.php?section=".$section."'>".$section."</a></td>
+							</tr>";
+							
+						/* outline for inserting links:
+						<td><a href='Student.php?StudentID=".$StudentID."'>".$StudentID."</a></td>
+						*/
+					}	
+			}
 			
-			echo "<table><tr><th>Lab Name</th><th>Lab begin date</th><th>Lab due date</th><th>Student Name</th><th>Student ID</th><th>Class Name</th><th>Class Section</th></tr>";
+			
+			/*
+			echo "<table><tr><th>Lab Name</th><th>Lab begin date</th><th>Lab due date</th><th>Student Name</th><th>Student ID</th><th>Class Name</th><th>Class Section</th><th>Grade</th><th>regrade?</th></tr>";
 			while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
 				$arr[] = $row;
-				list($labName, $BeginDate, $DueDate, $FirstName,$LastName, $StudentID, $className, $section) = $row;
+				list($labName, $BeginDate, $DueDate, $FirstName,$LastName, $StudentID, $className, $section, $grade) = $row;
 				echo "<tr><td>".$labName."</td>
 				<td>".$BeginDate."</td>
 				<td>".$DueDate."</td>
 				<td>".$FirstName." ".$LastName."</a></td>
 				<td>".$StudentID."</a></td>
 				<td>".$className."</td>
-				<td>".$section."</td></tr>";
-
+				<td>".$section."</td>
+				<td>".$grade."</td>
+				<td><a href='grade.php?studentID=".$StudentID."'>grading page</a></td></tr>";
+				
 				/* outline for inserting links:
 				<td><a href='Student.php?StudentID=".$StudentID."'>".$StudentID."</a></td>
 				*/
 
-			}
+			//}
+			
 			echo "</table>";
 			if(!$arr) exit('No rows');
 			//var_export($arr);
