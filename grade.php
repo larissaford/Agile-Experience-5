3d7b8e13592093d778c7ca54c2ecbcb99381149a
Jake <60721064+Jake-Derrick@users.noreply.github.com>
2020-04-29 18:40:23 -0500

    Added multiple types of grade entry

--- grade.php
+++ grade.php
@@ -1,6 +1,6 @@
 <?php
     // Database info
-    $DATABASE_HOST = '144.13.22.59:3306';
+    $DATABASE_HOST = 'localhost:3306';
     $DATABASE_USER = 'g5AppUser';
     $DATABASE_PASS = 'aug5';
     $DATABASE_NAME = 'G5AgileExperience';
@@ -13,6 +13,7 @@
         exit('Failed to connect: ' . mysqli_connect_error());
     }
 
+    echo("StudentID:" . $POST_['StudentID']);
     // Check that POST information is correct.
     if(!isset($_POST['StudentID'], $_POST['LabID'])) {
         exit('Failed to retrieve POST information.');
@@ -20,7 +21,7 @@
 
     // Get student and lab information based on POST data
     $stmt = $con->prepare(
-        'SELECT FirstName, LastName, Name, Rubric, BeginDate, DueDate
+        'SELECT FirstName, LastName, Name, Rubric, BeginDate, DueDate, GradeForCompletion
         FROM Student
         JOIN StudentSection ON Student.ID = StudentSection.StudentID
         JOIN Section ON Section.ID = StudentSection.SectionID
@@ -36,10 +37,9 @@
     );
     $stmt->bind_param('ii', $_POST['StudentID'], $_POST['LabID']);
     $stmt->execute();
-    $stmt->bind_result($FirstName, $LastName, $LabName, $Rubric, $BeginDate, $DueDate);
+    $stmt->bind_result($FirstName, $LastName, $LabName, $Rubric, $BeginDate, $DueDate, $GradeForCompletion);
     $stmt->fetch();
     $stmt->close();
-    $con->close();
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -55,13 +55,40 @@
             Due Date: <?=$DueDate?></br>
             Rubric: <?=$Rubric?></br>
         </p>
+
+        <?php
+        $stmt = $con->prepare(
+            'SELECT MaxScore, Criteria.Name, GradeForCompletion 
+            FROM Lab 
+            JOIN Criteria ON Lab.ID = Criteria.LabID 
+            WHERE Lab.ID = ?;'
+            );   
+        $stmt->bind_param('i', $_POST['LabID']);
+        $stmt->execute();
+
+        $stmt->bind_result($MaxScore, $CriteriaName, $GradeForCompletion);
+        ?>
+        <table>
+        <?php
+        while($stmt->fetch())
+        {
+            ?>
+                <tr><?=$CriteriaName?></tr>
+                    <td><input type="number" id="Grade" name="Grade" min="0" max="<?=$MaxScore?>" step="1" value="<?=$MaxScore?>"></td>
+            <?php
+        }
+        ?>
+        </table>
         <form onsubmit="return submitGrade(this)" method="post" autocomplete="off">
-            <label>Grade:</label>
-            <input type="number" id="Grade" name="Grade" min="0" max="100" step="1" value="100" required>
+            <input type="hidden" name="Grade" value="100">
             <input type="submit" value="Submit"><br>
             <p id="status"></p>
             <input type="hidden" name="StudentID" value="<?=$_POST['StudentID']?>"/>
             <input type="hidden" name="LabID" value="<?=$_POST['LabID']?>"/>
         </form>
+        <?php
+        $stmt->close();
+        $con->close();
+        ?>
     </body>
 </html>
