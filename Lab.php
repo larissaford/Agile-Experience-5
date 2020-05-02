ae4fd5fe7612849eb4472fd318dcb941376c1d3f
James Cerasani <fullmetalnerd97@gmail.com>
2020-05-02 12:49:40 -0500

    Made more changes to Lab.php including proper php placement

--- Lab.php
+++ Lab.php
@@ -38,68 +38,49 @@ Generates a page giving information about a single lab.
             $stmt->bindParam(':name', $_GET["labName"], PDO::PARAM_STR,64); //Accepting Input
             $stmt->execute();
             
-            $counter = 0;
+            $row = $stmt->fetch(PDO::FETCH_NUM);
+            list($labID, $labName, $beginDate, $dueDate, $rubric, $firstName, $lastName, $studentID, $className, $sectionNum) = $row;
+            ?>
+            <h1><?=$labName?> (Lab ID: <?=$labID?>)</h1>
+            <table>
+                <tr>
+                    <th>Begin Date</th>
+                    <th>Due Date</th>
+                </tr>
+                <tr>
+                    <td><?=$beginDate?></td>
+                    <td><?=$dueDate?></td>
+                </tr>
+            </table>
+
+            <p>
+            Link to Rubric: <a href='rubrics/<?=rubric?>'>Rubric</a>
+            </p>
+<?php
 			while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-                $arr[] = $row;
                 list($labID, $labName, $beginDate, $dueDate, $rubric, $firstName, $lastName, $studentID, $className, $sectionNum) = $row;
-                
-                if ($counter == 0) {
-                    echo "
-                        <h1>".$labName." (Lab ID: ".$labID.")</h1>
-                        <table>
-                            <tr>
-                                <th>Begin Date</th>
-                                <th>Due Date</th>
-                            </tr>
-                            <tr>
-                                <td>".$beginDate."</td>
-                                <td>".$dueDate."</td>
-                            </tr>
-                        </table>
 
-                        <p>
-                        Link to Rubric: <a href='rubrics/".$rubric."'>Rubric</a>
-                        </p>
-
-                        <table>
-                            <tr>
-                                <th>Student ID</th>
-                                <th>Student Name</th>
-                                <th>Class Name</th>
-                                <th>Section Number</th>
-                                
-                            </tr>
-                            <tr>
-                                <td><a href='Student.php?StudentID=".$studentID."'>".$studentID."</a></td>
-                                <td><a href='Student.php?StudentID=".$studentID."'>".$firstName." ".$lastName."</a></td>
-                                <td><a href='Class.php?className=".$className."'>".$className."</a></td>
-                                <td><a href='Class.php?className=".$className."'>".$sectionNum."</a></td>
-                                <td><a href='Grade.php?StudentID=".$studentID."&LabID=".$labID."'>Grade Lab</a></td>
-                            </tr>
-                    ";
-                    $counter++;
-                }
-                else {
-                    echo "
-                        <tr>
-                            <td><a href='Student.php?StudentID=".$studentID."'>".$studentID."</a></td>
-                            <td><a href='Student.php?StudentID=".$studentID."'>".$firstName." ".$lastName."</a></td>
-                            <td><a href='Class.php?className=".$className."'>".$className."</a></td>
-                            <td><a href='Class.php?className=".$className."'>".$sectionNum."</a></td>
-                            <td><a href='Grade.php?StudentID=".$studentID."&LabID=".$labID.">Grade Lab</a></td>
-                        </tr>
-                    ";
-                }
+                    ?>
+                    <tr>
+                        <td><a href='Student.php?StudentID=<?=$studentID?>'><?=$studentID?></a></td>
+                        <td><a href='Student.php?StudentID=<?=$studentID?>'><?=$firstName?> <?=$lastName?></a></td>
+                        <td><a href='Class.php?className=<?=$className?>'><?=$className?></a></td>
+                        <td><a href='Class.php?className=<?=$className?>'><?=$sectionNum?></a></td>
+                        <td><a href='Grade.php?StudentID=<?=$studentID?>&LabID=<?=$labID?>'>Grade Lab</a></td>
+                    </tr>
+                    <?php
             }
 
-            if ($counter == 0) { //never touched the loop, therefore there is some issue with this lab
-                echo "<p>
-                       This lab (".$_GET["labName"].") is not active or has no active students. Please add students or activate lab to continue.
-                      </p>";
+            if ($stmt->rowCount==0) { //never touched the loop, therefore there is some issue with this lab
+                ?>
+                <p>
+                       This lab (<?=$_GET["labName"]?>) is not active or has no active students. Please add students or activate lab to continue.
+                      </p>
+                      <?php
             }
+            ?>
+            <!--Notes form has no link to save text value.-->
             
-            // Notes form has no link to save text value.
-            echo "
             </table>
             <br \><br \>
             <div id='inputNotes' align='left'>
@@ -110,7 +91,7 @@ Generates a page giving information about a single lab.
                 <input type='submit' value='Submit'>
                 </form>
             </div>
-            ";
+            <?php
 		}
 		catch(PDOException $e) {
 			echo "Error: " . $e->getMessage();
