f6f0666b1e76801f9b22f63013c5696bdf52c8af
Larissa <larissa@coreyford.com>
2020-05-04 18:38:17 -0500

    fixed bugs with result.php and student.php, added links to rubrics

--- Student.php
+++ Student.php
@@ -39,6 +39,25 @@
                     GROUP BY Lab.ID , Lab.Name , BeginDate, DueDate, Rubric, FirstName, LastName, Student.ID, Class.Name , Section.SectionNum");
 			
 			$stmt->execute([$_GET['StudentID']]);
+
+
+			if ($stmt->rowCount() == 0) { //never touched the loop, therefore there is some issue with this lab
+				$stmt = $conn->prepare("
+				select Lab.ID labID, Lab.Name labName, BeginDate, DueDate, Rubric, FirstName, LastName, Student.ID, Class.Name className, Section.SectionNum section
+						from Student
+						inner JOIN StudentSection ON Student.ID = StudentSection.StudentID
+						inner JOIN Section ON Section.ID = StudentSection.SectionID
+						inner JOIN SectionLab ON Section.ID = SectionLab.SectionID
+						inner JOIN Lab ON Lab.ID = SectionLab.LabID
+						inner join Class on Section.ClassID=Class.ID
+						WHERE Student.IsActive 
+						AND StudentSection.IsActive 
+						AND Section.IsActive
+						AND SectionLab.IsActive
+						AND Lab.IsActive and Student.ID=?");
+				
+				$stmt->execute([$_GET['StudentID']]);
+			}
 			
 			$row = $stmt->fetch(PDO::FETCH_NUM);
 			//$arr[] = $row;
@@ -58,7 +77,7 @@
 					<td><a href='Lab.php?labName=".$labName."'>".$labID."</a></td>
 					<td>".$beginDate."</td>
 					<td>".$dueDate."</td>
-					<td>".$rubric."</td>
+					<td><a href='rubrics/".$rubric."'>".$rubric."</a></td>
 					<td>".$Grade."</td>
 					<td><a href='Grade.php?StudentID=".$studentID."&LabID=".$labID."'>Grading Page</td></tr>";
 			
