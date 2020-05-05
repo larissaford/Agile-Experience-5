85501217ed18ac3eb392265f49d2fcde830947fb
Larissa <larissa@coreyford.com>
2020-05-04 20:33:48 -0500

    changed the layout of student.php

--- Student.php
+++ Student.php
@@ -21,6 +21,7 @@
 		try {
 			$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
 			$stmt = $conn->prepare("
 			select Lab.ID labID, Lab.Name labName, BeginDate, DueDate, Rubric, FirstName, LastName, Student.ID, Class.Name className, Section.SectionNum section, Grade, max(Log.TimeStamp) mostRecent
 					from Student
@@ -37,11 +38,11 @@
 					AND SectionLab.IsActive
 					AND Lab.IsActive and Student.ID=?
                     GROUP BY Lab.ID , Lab.Name , BeginDate, DueDate, Rubric, FirstName, LastName, Student.ID, Class.Name , Section.SectionNum");
-			
 			$stmt->execute([$_GET['StudentID']]);
 
 
 			if ($stmt->rowCount() == 0) { //never touched the loop, therefore there is some issue with this lab
+				
 				$stmt = $conn->prepare("
 				select Lab.ID labID, Lab.Name labName, BeginDate, DueDate, Rubric, FirstName, LastName, Student.ID, Class.Name className, Section.SectionNum section
 						from Student
@@ -55,36 +56,70 @@
 						AND Section.IsActive
 						AND SectionLab.IsActive
 						AND Lab.IsActive and Student.ID=?");
-				
 				$stmt->execute([$_GET['StudentID']]);
 			}
 			
 			$row = $stmt->fetch(PDO::FETCH_NUM);
-			//$arr[] = $row;
 			list($labID, $labName, $beginDate, $dueDate, $rubric, $firstName, $lastName, $studentID, $className, $section, $Grade, $timeStamp) = $row;
 			?>
 			
 			<h1><?=$firstName?> <?=$lastName?> (Student ID: <?=$studentID?>)</h1>
-			<table><tr><th>Class Name</th><th>Class Section</th><th>Lab Name</th><th>Lab ID</th><th>Lab begin date</th><th>Lab due date</th><th>Lab Rubric</th><th>Lab Grade</th><th>regrade?</th></tr>	
+			
+			Classes:
+			<table><tr><th>Class Name</th><th>Class Section</th></tr>
+			
+			<?php
+				$stmt2 = $conn->prepare("
+				select Lab.ID labID, Lab.Name labName, BeginDate, DueDate, Rubric, FirstName, LastName, Student.ID, Class.Name className, Section.SectionNum section
+						from Student
+						inner JOIN StudentSection ON Student.ID = StudentSection.StudentID
+						inner JOIN Section ON Section.ID = StudentSection.SectionID
+						inner JOIN SectionLab ON Section.ID = SectionLab.SectionID
+						inner JOIN Lab ON Lab.ID = SectionLab.LabID
+						inner join Class on Section.ClassID=Class.ID
+						WHERE Student.IsActive 
+						AND StudentSection.IsActive 
+						AND Section.IsActive
+						AND SectionLab.IsActive
+						AND Lab.IsActive and Student.ID=?
+                        group by Class.Name, Section.SectionNum");
+				$stmt2->execute([$_GET['StudentID']]);
+
+				while ($row = $stmt2->fetch(PDO::FETCH_NUM)) {
+					list($labID, $labName, $beginDate, $dueDate, $rubric, $firstName, $lastName, $studentID, $className, $section) = $row;
+					
+					echo "<tr>
+						<td><a href='Class.php?className=".$className."'>".$className."</a></td>
+						<td>".$section."</td>
+						</tr>";
+				
+				}
+				echo "</table>";
+			?>
+
+			Labs:
+			<table><tr><th>Lab Name</th><th>Lab ID</th><th>Lab begin date</th><th>Lab due date</th><th>Lab Rubric</th><th>Lab Grade</th><th>regrade?</th></tr>	
 
 			<?php
-			do {
-				list($labID, $labName, $beginDate, $dueDate, $rubric, $firstName, $lastName, $studentID, $className, $section, $Grade) = $row;
+			$stmt->execute([$_GET['StudentID']]);
+
+			while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
+				list($labID, $labName, $beginDate, $dueDate, $rubric, $firstName, $lastName, $studentID, $className, $section, $Grade, $timeStamp) = $row;
 				
-				echo "<tr><td><a href='Class.php?className=".$className."'>".$className."</a></td>
-					<td>".$section."</td>
-					<td><a href='Lab.php?labName=".$labName."'>".$labName."</a></td>
+                echo "<tr>
+                    <td><a href='Lab.php?labName=".$labName."'>".$labName."</a></td>
 					<td><a href='Lab.php?labName=".$labName."'>".$labID."</a></td>
 					<td>".$beginDate."</td>
 					<td>".$dueDate."</td>
 					<td><a href='rubrics/".$rubric."'>".$rubric."</a></td>
 					<td>".$Grade."</td>
-					<td><a href='Grade.php?StudentID=".$studentID."&LabID=".$labID."'>Grading Page</td></tr>";
+					<td><a href='Grade.php?StudentID=".$studentID."&LabID=".$labID."'>Grading Page</td>
+					</tr>";
 			
-			} while ($row = $stmt->fetch(PDO::FETCH_NUM));
-
-			echo "</table>";
+            }
+            echo "</table>";
 			$stmt = null;
+			$stmt2 = null;
 			
 		}
 		catch(PDOException $e) {
